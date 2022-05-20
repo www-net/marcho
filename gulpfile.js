67e1ad7559a70abffcba117ec0dc26cf5bd89d0f
www-net <78052177+www-net@users.noreply.github.com>
2022-05-20 15:17:05 +0300

    added shop page

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,9 @@ function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
         'app/js/main.js'
     ])
